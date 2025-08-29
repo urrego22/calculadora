a262827549fa8987ad357bf46143226147d7e784
Sara Garcia <urregosara984@gmail.com>
2025-08-29 16:15:05 -0500

    implementacion de la funcion de multiplicar

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function restar(num1, num2) {
   return num1 - num2;
 }
 
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
+
+
 
 // Función principal del programa
 function calculadora() {
