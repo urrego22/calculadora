0467185b1f96ab264bb9eeaa19644bc37a6fee66
Sara Garcia <urregosara984@gmail.com>
2025-08-29 16:21:12 -0500

    implementacion de la funcion de la division

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function multiplicar(num1, num2) {
   return num1 * num2;
 }
 
+function dividir(num1, num2) {
+  if (num2 === 0) {
+    return "Error: división por cero";
+  }
+  return num1 / num2;
+}
+
+
 
 
 // Función principal del programa
